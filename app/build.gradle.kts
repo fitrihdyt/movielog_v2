daedcd74dde5fe75eba9ae4af7f8dd0e441289d9
Fitri Nurhidayat <fitrinurhidayat42@gmail.com>
2026-06-19 18:28:34 +0700

    Menyiapkan API Key Google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
@@ -20,6 +22,15 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+
+        buildConfigField(
+            "String",
+            "API_KEY",
+            properties.getProperty("API_KEY")
+        )
     }
 
     buildTypes {
@@ -36,6 +47,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
     buildFeatures {
+        buildConfig = true
         compose = true
     }
 }
